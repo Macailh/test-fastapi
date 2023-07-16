01eaf4c30ca52f0b39ae7452678de5505efbe7e9
Macailh <salvadorenriquegb@gmail.com>
2023-07-15 21:09:28 -0600

    Add validation with external method

--- path_params/main.py
+++ path_params/main.py
@@ -4,6 +4,10 @@ from fastapi import FastAPI, Path
 app = FastAPI()
 
 
+def integer_gt_zero_param(default: int = Path(..., gt=0)):
+    return default
+
+
 @app.get("/items/{item_id}")
 def read_item_(item_id: int = Path(..., title="ID del artículo", description="El ID único del artículo a recuperar", gt=0)):
     return {"item_id": item_id}
@@ -13,3 +17,13 @@ def read_item_(item_id: int = Path(..., title="ID del artículo", description="E
 def read_user_item(user_id: int = Path(..., title="ID del usuario", gt=0),
                    item_id: int = Path(..., title="ID del artículo", gt=0)):
     return {"user_id": user_id, "item_id": item_id}
+
+
+@app.get("/users/{user_id}")
+def get_user_by_id(user_id: int = integer_gt_zero_param()):
+    return {"user_id": user_id}
+
+
+@app.get("/products/{produc_id}")
+def get_product_by_id(produc_id: int):
+    return {"product_id": produc_id}
